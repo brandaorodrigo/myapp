ab408ef96eeff3cd1fd360a197c6cbfc9bd4d00c
rodrigo brandão <rodrigo.brandao@spotmetrics.com>
2021-10-30 01:09:42 -0300

    spot

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.scss';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/lib/locale/pt_BR';
 
-export default App;
+import { Home, LoggedTemplate, Login, Logout, PublicTemplate } from './Example';
+import { StorageProvider, useStorageContext } from './react-storage-state';
+
+import 'antd/dist/antd.css';
+
+export const VERSION = '21.11';
+
+function LoggedRouter() {
+    return (
+        <LoggedTemplate>
+            <Switch>
+                <Route component={Home} exact path="/about/:id/:idd" />
+                <Route component={Home} exact path="/about/:id" />
+                <Route component={Home} exact path="/about" />
+                <Route component={Logout} exact path="/logout" />
+                <Route component={Home} exact path="/" />
+                {/* 404 ---------------------------------------------------- */}
+                <Route render={() => <Redirect to="/logout" />} />
+            </Switch>
+        </LoggedTemplate>
+    );
+}
+
+function PublicRouter() {
+    return (
+        <PublicTemplate>
+            <Switch>
+                <Route component={Login} exact path="/" />
+                {/* 404 ---------------------------------------------------- */}
+                <Route render={() => <Redirect to="/" />} />
+            </Switch>
+        </PublicTemplate>
+    );
+}
+
+function Router() {
+    const useStorage = useStorageContext();
+    const [token] = useStorage('token');
+    return (
+        <BrowserRouter>
+            {token ? <LoggedRouter /> : <PublicRouter />}
+        </BrowserRouter>
+    );
+}
+
+export default function App(): React.ReactElement {
+    return (
+        <StorageProvider>
+            <ConfigProvider locale={ptBR}>
+                <Router />
+            </ConfigProvider>
+        </StorageProvider>
+    );
+}
---
+++ src/Example.tsx
@@ -0,0 +1,143 @@
+import React, { useCallback, useEffect } from 'react';
+import { NavLink, useHistory, useLocation, useParams } from 'react-router-dom';
+
+import { Button, Form, Input } from 'antd';
+
+import { useStorageContext } from './react-storage-state';
+
+/* ========================================================================== */
+/* ========================================================================== */
+/* TEMPLATE ================================================================= */
+/* ========================================================================== */
+
+export const LoggedTemplate: React.FC = ({ children }) => (
+    <div>
+        <div style={{ padding: '20px', background: '#dd88aa' }}>
+            <NavLink activeClassName="active" to="/">
+                HOME
+            </NavLink>
+        </div>
+        <div style={{ padding: '20px', background: '#dd88aa' }}>
+            <NavLink activeClassName="active" to="/about">
+                ABOUT
+            </NavLink>
+        </div>
+        <div style={{ padding: '20px', background: '#aa65aa' }}>
+            <NavLink to="/logout">SAIR</NavLink>
+        </div>
+
+        <div style={{ background: '#0ee' }}>{children}</div>
+    </div>
+);
+
+export const PublicTemplate: React.FC = ({ children }) => (
+    <div style={{ background: '#ee0' }}>{children}</div>
+);
+
+/* ========================================================================== */
+/* PAGES ==================================================================== */
+/* ========================================================================== */
+
+export const Login: React.FC = () => {
+    const useStorage = useStorageContext();
+    const [, setToken] = useStorage('token');
+    const [, setMall] = useStorage('mall');
+
+    const history = useHistory();
+
+    const onFinish = () => {
+        setToken('tokenValue');
+        setMall('mallValue');
+        history.push('/');
+    };
+
+    return (
+        <Form onFinish={onFinish}>
+            <Form.Item label="Username" name="username">
+                <Input />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+                <Input.Password />
+            </Form.Item>
+            <Form.Item>
+                <Button htmlType="submit">Submit</Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export const Logout: React.FC = () => {
+    const useStorage = useStorageContext();
+    const [, setToken] = useStorage('token');
+    const [, setMall] = useStorage('mall');
+
+    const history = useHistory();
+
+    const handleLogout = useCallback(() => {
+        setToken(undefined);
+        setMall(undefined);
+        history.push('/');
+    }, [history, setToken, setMall]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            handleLogout();
+        }, 1000);
+    }, [handleLogout]);
+
+    return <div />;
+};
+
+export const Home: React.FC = () => {
+    const useStorage = useStorageContext();
+    const [token, setToken] = useStorage('token');
+    const [mall, setMall] = useStorage('mall');
+
+    const { search, hash } = useLocation();
+
+    const history = useHistory();
+
+    const query = new URLSearchParams(search);
+
+    const { pathname } = useLocation();
+
+    const { id, idd } = useParams<any>();
+
+    return (
+        <h2>
+            QUERY: {query.get('teste')}
+            <br />
+            HASH: {hash}
+            <br />
+            ID: {id}
+            <br />
+            IDD: {idd}
+            <br />
+            <br />
+            STATE-TOKEN: {token}
+            <br />
+            <br />
+            MALL: {mall}
+            <br />
+            localStorage: {localStorage.getItem('token')}
+            <br />
+            <button onClick={() => setToken('newTokenValue')} type="button">
+                SET NEW TOKEN
+            </button>
+            <button onClick={() => setMall('newMallValue')} type="button">
+                SET NEW MALL
+            </button>
+            <button
+                onClick={() => {
+                    history.push('/reload');
+                    history.replace(pathname);
+                }}
+                type="button"
+            >
+                RELOAD PAGE
+            </button>
+            <br />
+            <NavLink to="/about/dsdsd?teste=444">TESTE SEARCH</NavLink>
+        </h2>
+    );
+};
