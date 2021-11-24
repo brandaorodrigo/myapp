26fe2e198771c8b659510aedfd419480936a75f7
rodrigo brandão <rodrigo.brandao@spotmetrics.com>
2021-11-24 03:52:00 -0300

    loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,16 +1,16 @@
 import './Loading.css';
 
 const Loading: React.FC<{
-    status?: boolean;
-    opacity?: string;
-    style?: React.CSSProperties;
     curtainStyle?: React.CSSProperties;
+    opacity?: string;
+    status?: boolean;
+    style?: React.CSSProperties;
 }> = ({ status = false, style, opacity = '0.8', curtainStyle }) => (
     <div
         className="ant-loading"
         style={{
-            display: status ? 'block' : 'none',
             background: `rgba(255, 255, 255, ${opacity})`,
+            display: status ? 'block' : 'none',
             ...curtainStyle,
         }}
     >
